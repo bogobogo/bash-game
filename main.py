9cd59689493014fd927a26ce8cdb56e746701cc2
Elad Bogomolny <eladbo@wix.com>
2018-05-30 18:17:18 -0400

    fixes after pairing with andy

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+#!/usr/bin/python
 import sys
 import subprocess
 import os
 import fileinput
+
 def removeNewLine(s):
-    if repr(s)[-3:-1] == '\\n':
-        return repr(s)[1:-3]
-    return str(s)
+    if s.endswith('\n'):
+        return s[:-1] 
+    return s
 
-sys.stdout.write('\033[95m' + "level 1, game >" + '\033[94m')
-x = sys.stdin.readline()
-print len(x)
-print repr(x)[1:-3]
-print repr(x)[-3:-1] == "\\n"
-sys.stdout.write(removeNewLine(x))
-#print x[0:-3]
-y = subprocess.check_output([removeNewLine(x)])
-print y
-#print os.environ['HOME']
-#print subprocess
-
+while True:
+    sys.stdout.write('\033[95m' + "level 1,\rHi game >" + '\033[94m')
+    x = sys.stdin.readline()
+    command = removeNewLine(x)
+    try:
+        y = subprocess.call(command, shell=True)
+        n = subprocess.call("echo $PS1", shell=True)
+        print os.getcwd()
+        print n
+        if command == 'cd ..':
+            os.chdir('..')
+        print y
+    except Exception as e:
+        print 'Exception: %s' % e
---
+++ utils.py
@@ -0,0 +1,10 @@
+
+def removeNewLine(s):
+    if repr(s)[-3:-1] == '\\n':
+        return repr(s)[1:-3]
+    return str(s)
+
+
+
+
+
