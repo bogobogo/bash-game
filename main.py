b325b3406b5c66b7b413f74174352e97883b9649
Elad Bogomolny <eladbo@wix.com>
2018-05-30 14:43:14 -0400

    small pos of calling ls and printing the result

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print 1
+import sys
+import subprocess
+import os
+import fileinput
+sys.stdout.write('\033[95m' + "level 1, game >" + '\033[94m')
+x = sys.stdin.readline()
+y = subprocess.check_output(["ls"])
+print y
+print os.environ['HOME']
+print subprocess
+sys.stdout.write("${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)")
+
+print sys.argv[1]
