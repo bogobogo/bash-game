ea907c452eecfea1545163abf7b4a97d987f1268
Elad Bogomolny <eladbo@wix.com>
2018-05-30 15:30:04 -0400

    first poc of reading command from reading, executing it, and printing result

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import sys
 import subprocess
 import os
 import fileinput
+def removeNewLine(s):
+    if repr(s)[-3:-1] == '\\n':
+        return repr(s)[1:-3]
+    return str(s)
+
 sys.stdout.write('\033[95m' + "level 1, game >" + '\033[94m')
 x = sys.stdin.readline()
-y = subprocess.check_output(["ls"])
+print len(x)
+print repr(x)[1:-3]
+print repr(x)[-3:-1] == "\\n"
+sys.stdout.write(removeNewLine(x))
+#print x[0:-3]
+y = subprocess.check_output([removeNewLine(x)])
 print y
-print os.environ['HOME']
-print subprocess
-sys.stdout.write("${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)")
+#print os.environ['HOME']
+#print subprocess
 
-print sys.argv[1]
