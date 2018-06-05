ae5961ba116c97bd20b638ba3097fb8b590bc017
Elad Bogomolny <eladbo@wix.com>
2018-06-05 11:07:27 -0400

    attempt at printing line by line with a for loop - too slow and problematic for characters that depend on the next line

--- src/messages.py
+++ src/messages.py
@@ -8,23 +8,23 @@ class Message():
     def createDeleteMessage(message, x):
         split_message = removeNewLine(message).split("\n")
         erased_split_message = [re.sub(r'.', " ", msg) for msg in split_message]
-        base = "\n%-mamas".replace("mama", str(x))
-        return base.join(erased_split_message)
+        return era
     @staticmethod
     def createMessage(message, x):        
         split_message = removeNewLine(message).split("\n")
-        base = "\n%-mamas".replace("mama", str(x))
-        return base.join(split_message)
+        return split_message
     def __init__(self, message, id, startPosition):
         self.id = id
         self.message = self.createMessage(message, startPosition[0])
         self.deleteMessage = self.createDeleteMessage(message, startPosition[0])
         self.startPosition = startPosition
     def write(self):
-        printMessageAt(self.message, self.startPosition)
+        for idx, line in enumerate(self.message):
+            printMessageAt(line, (self.startPosition[0], self.startPosition[1] + idx )) 
+        
     def erase(self):
-        # print repr(self.deleteMessage)
-        printMessageAt(self.deleteMessage, (self.startPosition[0],self.startPosition[1]-1) )
+        for idx, line in enumerate(self.deleteMessage):
+            printMessageAt(line, (self.startPosition[0], self.startPosition[1] + idx )) 
         
 
 
