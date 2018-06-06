bd841041f8c279fc3c66e972a953d5195993ced9
Elad Bogomolny <eladbo@wix.com>
2018-06-06 18:17:01 -0400

    game creation api concept

---
+++ src/game.py
@@ -0,0 +1,13 @@
+
+levels = {"1" : 100, 
+          "2": 400, 
+          "3": 1000}
+
+def createMission(desiredOutput, desiredInput, messages, xp, errorMessages):
+    pass
+
+mission1 = createMission()
+mission2 = createMission()
+missions = [mission1, mission2, mission3]
+
+game(missions)
